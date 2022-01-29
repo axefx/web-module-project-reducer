8a5f43166c552135e45f78c63356479b9b548878
Axel <axacorro@gmail.com>
2022-01-29 15:16:06 -0800

    imports reducer and initialstate to use with useReducer hook

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
-import React from 'react';
+import React, { useReducer } from 'react';
 
 import './App.css';
 
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
+import reducer, { initialState } from '../reducers';
 
 function App() {
+  // use useReducer to access state and dispatch
+  const [state, dispatch] = useReducer(reducer, initialState)
+
   return (
     <div className="App">
       <nav className="navbar navbar-dark bg-dark">
