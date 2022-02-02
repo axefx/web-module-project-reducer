312dcbe63d68c0dde0b02f496b1f09a109867bb8
Axel <axacorro@gmail.com>
2022-02-01 23:54:15 -0800

    implements event handler for MC calc button to clear memory

--- src/components/App.js
+++ src/components/App.js
@@ -5,7 +5,7 @@ import './App.css';
 import TotalDisplay from './TotalDisplay';
 import CalcButton from './CalcButton';
 import reducer, { initialState } from '../reducers';
-import { applyNumber, updateOperator, clearTotal, saveMemory, requestMemory } from '../actions';
+import { applyNumber, updateOperator, clearTotal, saveMemory, requestMemory, clearMemory } from '../actions';
 
 function App() {
   // use useReducer to access state and dispatch
@@ -33,7 +33,7 @@ function App() {
             <div className="row">
               <CalcButton value={"M+"} onClick={(e) => dispatch(saveMemory(state.total))}/>
               <CalcButton value={"MR"} onClick={(e) => dispatch(requestMemory())}/>
-              <CalcButton value={"MC"}/>
+              <CalcButton value={"MC"} onClick={(e) => dispatch(clearMemory())}/>
             </div>
 
             <div className="row">
